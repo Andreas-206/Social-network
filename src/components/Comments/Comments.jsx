15646bb3351fd8273cdf556f1ee042b13c3db851
ValeriiaProkhina <applehouse1883@icloud.com>
2024-04-06 12:12:25 +0300

    creaateAPI

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Comment } from '../Comment/Comment';
-import { Grid } from '../Grid/Grid';
-import { comments } from '../../helpers/comments';
-import { selectFilter } from '../../redux/filterSlice';
-import { useSelector } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { Comment } from "../Comment/Comment";
+import { Grid } from "../Grid/Grid";
+// import { comments } from "../../helpers/comments";
+import { selectFilter } from "../../redux/filterSlice";
+import { useSelector } from "react-redux";
+import { useGetCommentsQuery } from "../../redux/commentApi";
 
 export const Comments = () => {
-    const filter = useSelector(selectFilter);
+  const { data: comments, isLoading } = useGetCommentsQuery();
+  const filter = useSelector(selectFilter);
 
-    console.log(filter);
-    const filteredComments = comments.filter((comment) =>
-        comment.content.toLowerCase().includes(filter.toLowerCase())
-    );
+  const filteredComments = comments?.filter((comment) =>
+    comment.content.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    return (
-        <Grid>
-            {comments &&
-                filteredComments.map((comment) => (
-                    <Comment key={comment.id} {...comment} />
-                ))}
-        </Grid>
-    );
+  return (
+    <Grid>
+      {comments &&
+        filteredComments.map((comment) => (
+          <Comment key={comment.id} {...comment} />
+        ))}
+    </Grid>
+  );
 };
 
 Comments.propTypes = {
-    comments: PropTypes.arrayOf(PropTypes.shape().isRequired),
+  comments: PropTypes.arrayOf(PropTypes.shape().isRequired),
 };
--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,61 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = 'https://65f6b8a641d90c1c5e0b3247.mockapi.io/api';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://65f6b8a641d90c1c5e0b3247.mockapi.io/api";
 
 export const commentApi = createApi({
-    reducerPath: 'comments',
-    baseQuery: fetchBaseQuery({
-        baseUrl: BASE_URL,
+  reducerPath: "comments",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+      providesTags: ["Comments"],
     }),
-    tagTypes: ['Comments'],
-    endpoints: (builder) => ({}),
+    addComment: builder.mutation({
+      query: (data) => ({
+        url: API_ENDPOINT,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Comments"],
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery, useAddCommentMutation } = commentApi;
+
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// const api = createApi({
+//   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+//   tagTypes: ['Posts'],
+//   endpoints: (build) => ({
+//     getPosts: build.query({
+//       query: () => 'posts',
+//       providesTags: (result) =>
+//         result ? result.map(({ id }) => ({ type: 'Posts', id })) : [],
+//     }),
+//     addPost: build.mutation({
+//       query: (body) => ({
+//         url: `posts`,
+//         method: 'POST',
+//         body,
+//       }),
+//       invalidatesTags: ['Posts'],
+//     }),
+//   }),
+// })
+
+// Auto-generated hooks
+// export const { useGetPostsQuery, useAddPostMutation } = api
+
+// Possible exports
+// export const { endpoints, reducerPath, reducer, middleware } = api
+// reducerPath, reducer, middleware are only used in store configuration
+// endpoints will have:
+// endpoints.getPosts.initiate(), endpoints.getPosts.select(), endpoints.getPosts.useQuery()
+// endpoints.addPost.initiate(), endpoints.addPost.select(), endpoints.addPost.useMutation()
+// see `createApi` overview for _all exports_
